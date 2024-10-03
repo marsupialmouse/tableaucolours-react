bb2d9bb73c6fa13c0120d7002de7503821f626c3
marsupialmouse <16873013+marsupialmouse@users.noreply.github.com>
2024-10-03 16:12:14 +1000

    Add Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
+import { defineConfig as defineTestConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const config = defineConfig({
+  plugins: [react()], 
 })
+
+const testConfig = defineTestConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-setup.ts'],
+  },
+})
+
+export default mergeConfig(config, testConfig)
