b2c3b477290107ded415c89ab8f400fb62e47004
marsupialmouse <16873013+marsupialmouse@users.noreply.github.com>
2025-04-09 14:48:37 +1000

    Add TpsWriter

---
+++ src/utils/TpsWriter.test.ts
@@ -0,0 +1,111 @@
+import {PaletteTypes} from 'src/state/ColourPalettes/PaletteTypes'
+import {describe, expect, it} from 'vitest'
+import {colourPaletteXml, replacePalettesInTpsXml} from './TpsWriter'
+
+describe('Generates TPS XML colour palette', () => {
+  it('including name, type and colours', () => {
+    const name = 'Midweek Mayhem'
+    const type = PaletteTypes.sequential
+    const colours = ['#ffffff', '#00ff00', '#000000']
+
+    const xml = colourPaletteXml({name, type, colours})
+
+    expect(xml).toBe(`<color-palette name="${name}" type="${type.id}">
+    <color>#ffffff</color>
+    <color>#00ff00</color>
+    <color>#000000</color>
+</color-palette>`)
+  })
+
+  it('encodes name in XML', () => {
+    const xml = colourPaletteXml({
+      name: '<xml "name">',
+      type: PaletteTypes.regular,
+      colours: ['#fff'],
+    })
+
+    expect(xml)
+      .toBe(`<color-palette name="&lt;xml &quot;name&quot;&gt;" type="regular">
+    <color>#fff</color>
+</color-palette>`)
+  })
+
+  it('includes unkown string type', () => {
+    const xml = colourPaletteXml({name: 'X', type: 'what?', colours: ['#fff']})
+
+    expect(xml).toBe(`<color-palette name="X" type="what?">
+    <color>#fff</color>
+</color-palette>`)
+  })
+
+  it('includes hex value of Colour instances', () => {
+    const xml = colourPaletteXml({
+      name: 'X',
+      type: PaletteTypes.regular,
+      colours: [{hex: '#aa00bb', id: 1, isSelected: false}],
+    })
+
+    expect(xml).toBe(`<color-palette name="X" type="regular">
+    <color>#aa00bb</color>
+</color-palette>`)
+  })
+})
+
+describe('Replaces colour palettes in TPS XML', () => {
+  it('replaces existing colour paletes', () => {
+    const palettes = [
+      {name: 'Max', type: PaletteTypes.regular, colours: ['#f0f']},
+      {
+        name: 'David',
+        type: 'tools-out',
+        colours: [{hex: '#0f0', isSelected: false, id: 9}],
+      },
+    ]
+    const originalXml = `<?xml version='1.0'?>
+
+<workbook>
+	<preferences>
+		<color-palette name="Pablo Honey" type="regular">
+			<color>#FFFFFF</color>
+        </color-palette>
+    </preferences>
+</workbook>`
+
+    const newXml = replacePalettesInTpsXml(originalXml, palettes)
+    console.info(newXml)
+    expect(newXml).toBe(`<workbook>
+	<preferences>
+<color-palette name="Max" type="regular">
+    <color>#f0f</color>
+</color-palette>
+<color-palette name="David" type="tools-out">
+    <color>#0f0</color>
+</color-palette>
+</preferences>
+</workbook>`)
+  })
+
+  it('does not replace other preferences', () => {
+    const palettes = [
+      {name: 'Brakes', type: PaletteTypes.regular, colours: ['#f0f']},
+    ]
+    const originalXml = `<workbook>
+	<preferences>
+        <brakes>Delta Brakes</brakes>
+		<color-palette name="Pablo Honey" type="regular">
+			<color>#FFFFFF</color>
+        </color-palette>
+    </preferences>
+</workbook>`
+
+    const newXml = replacePalettesInTpsXml(originalXml, palettes)
+    console.info(newXml)
+    expect(newXml).toBe(`<workbook>
+	<preferences><brakes>Delta Brakes</brakes>
+<color-palette name="Brakes" type="regular">
+    <color>#f0f</color>
+</color-palette>
+</preferences>
+</workbook>`)
+  })
+})
---
+++ src/utils/TpsWriter.ts
@@ -0,0 +1,44 @@
+import {Colour} from 'src/state/ColourPalettes/PaletteReducer'
+import {PaletteType} from 'src/state/ColourPalettes/PaletteTypes'
+import he from 'he'
+
+export interface TpsColourPalette {
+  name: string
+  type: PaletteType | string
+  colours: Array<Colour> | Array<string>
+}
+
+const xmlParser = new DOMParser()
+const xmlSerializer = new XMLSerializer()
+
+function replacePalettesInTpsXml(
+  xml: string,
+  palettes: Array<TpsColourPalette>
+) {
+  const doc = xmlParser.parseFromString(xml, 'application/xml')
+  const preferences = doc.getElementsByTagName('preferences')[0]
+  const paletteElements = [...preferences.getElementsByTagName('color-palette')]
+
+  paletteElements.forEach((e) => e.remove())
+
+  const paletteXml = palettes.map((x) => colourPaletteXml(x)).join('\n')
+
+  preferences.innerHTML =
+    preferences.innerHTML.trim() + '\n' + paletteXml + '\n'
+
+  return xmlSerializer.serializeToString(doc)
+}
+
+function colourPaletteXml({name, type, colours}: TpsColourPalette) {
+  let x = `<color-palette name="${he.encode(name, {
+    useNamedReferences: true,
+  })}" type="${type instanceof PaletteType ? type.id : type}">\n`
+
+  colours.forEach(
+    (c) => (x += `    <color>${typeof c == 'string' ? c : c.hex}</color>\n`)
+  )
+
+  return x + '</color-palette>'
+}
+
+export {colourPaletteXml, replacePalettesInTpsXml}
