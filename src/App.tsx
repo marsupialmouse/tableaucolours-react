8910bf4a8491be222656ea491702e3ceae0a330d
marsupialmouse <16873013+marsupialmouse@users.noreply.github.com>
2025-05-27 17:12:16 +1000

    Fix editor layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
 import './App.less'
-import ColourPaletteColourList from './components/ColourPaletteColourList/ColourPaletteColourList'
-import {ColourPaletteType, ColourPaletteTypes} from './types/ColourPaletteTypes'
-import ColourPaletteTypeSelector from './components/ColourPaletteTypeSelector/ColourPaletteTypeSelector'
-import {useState} from 'react'
-import ColourPalettePreview from './components/ColourPalettePreview/ColourPalettePreview'
-import {useSelector} from 'react-redux'
-import {selectColourPaletteColours} from './stores/colourPaletteSlice'
+import ColourPaletteEditor from './components/ColourPaletteEditor/ColourPaletteEditor'
 
 function App() {
-  const [selectedType, setSelectedType] = useState(ColourPaletteTypes.sequential)
-
-  const colours = useSelector(selectColourPaletteColours)
-
-  function handleTypeSelected(type: ColourPaletteType): void {
-    setSelectedType(type)
-  }
-
   return (
     <>
-      <div style={{width: '20rem'}}>
-        <ColourPaletteTypeSelector
-          selectedType={selectedType}
-          onTypeSelected={handleTypeSelected}
-          tabIndex={1}
-        />
-        <ColourPaletteColourList />
-        <ColourPalettePreview type={selectedType} colours={colours} />
-      </div>
+      <section id="palettesection" className="overlay">
+        <div>
+          <ColourPaletteEditor />
+        </div>
+      </section>
     </>
   )
 }
